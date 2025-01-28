869a963139ffdeb417ea6730fb3d4d928aa9748e
Terrance Johnson Jr <terrance.johnson@nwktc.edu>
2025-01-28 10:35:01 -0700

    Initial Commit

---
+++ GrandmaPoints/GrandmaPointsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrandmaPointsApp.swift
+//  GrandmaPoints
+//
+//  Created by Terrance Johnson Jr on 1/28/25.
+//
+
+import SwiftUI
+
+@main
+struct GrandmaPointsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GrandmaPointsTests/GrandmaPointsTests.swift
@@ -0,0 +1,17 @@
+//
+//  GrandmaPointsTests.swift
+//  GrandmaPointsTests
+//
+//  Created by Terrance Johnson Jr on 1/28/25.
+//
+
+import Testing
+@testable import GrandmaPoints
+
+struct GrandmaPointsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
